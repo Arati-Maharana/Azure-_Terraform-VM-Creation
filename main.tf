2f062ad043bc768d7acdbf41d34d29eb115065b3
unknown <arati@gmail.com>
2024-08-21 00:32:27 +0530

    hello

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+
+resource "azurerm_resource_group" "rgblock" {
+  name= "aratirg"
+  location = "west us"
+}
+
+
+
 resource "azurerm_resource_group" "rgblock" {
   name= "aratirg"
   location = "west us"
